6ec3651c4c15d31cf5fa132111807d7f10994f92
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-06-13 17:38:58 +0200

    Export randiv

--- src/ModelsGenerators.jl
+++ src/ModelsGenerators.jl
@@ -1,5 +1,8 @@
 module ModelsGenerators
 
+export
+  randiv
+
 # package code goes here
 function randiv(n::Int64        = 100;
                 m::Int64        = 5,
@@ -26,3 +29,51 @@ function randiv(n::Int64        = 100;
 end
 
 end # module
+
+
+## function typed_fad_hessian{T<:Real}(f::Function, ::Type{T})
+##   g(x::Vector{T}) = hessian(f(FADHessian(x)))
+##   return g
+## end
+
+## function ForwardDiff.hessian{T<:Real, n}(x::Array{FADHessian{T, n},1})
+
+##     y = Array(T, n, n, length(x))
+##     k = 1
+
+##     for s = 1:length(x)
+##         for i in 1:n
+##             for j in 1:i
+##                 y[i, j, s] = x[s].h[k]
+##                 k += 1
+##             end
+##         end
+##         for i in 1:n
+##             for j in (i+1):n
+##                 y[i, j, s] = y[j, i, s]
+##             end
+##         end
+##         k = 1
+##     end
+    
+##     y
+## end
+
+## y,x,z = randiv(k=2)
+## h(theta) = (z'*(y-x*theta))[:]
+## h([.1,.1])
+
+## g = forwarddiff_jacobian(h, Float64, fadtype=:typed)
+## g([.1,.1])
+
+## fad =  hessian(f(FADHessian([.1, .1])))
+
+
+
+## f(x) = [x[1]^2+x[2], 3*x[1], x[1]^2*x[2]^3]
+
+## ss = forwarddiff_hessian(f, Float64, fadtype=:typed)
+## ss([.1, .1])
+
+## ss = forwarddiff_hessian(f, Float64, fadtype=:typed)
+## ss([.1, .1])
