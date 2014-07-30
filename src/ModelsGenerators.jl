d3bd2a4ee3e7c5d01a49d543e102db42a3823e8e
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-07-30 17:57:19 +0200

    Fix

--- src/ModelsGenerators.jl
+++ src/ModelsGenerators.jl
@@ -12,13 +12,13 @@ function randiv(n::Int64        = 100;
                 CP::Int64       = 10)
 
     ## Generate IV Model with CP
-    
+
     tau     = fill(sqrt(CP/(m*n)), m)
     z       = randn(n, m)
     vi      = randn(n, 1)
     eta     = randn(n, 1)
     epsilon = rho*eta+sqrt(1-rho^2)*vi
-    x       = z*tau + eta
+    x       = z*tau .+ eta
     if k>1
         x_ex = randn(n, k-1)
         x = [x  x_ex]
@@ -55,7 +55,7 @@ end # module
 ##         end
 ##         k = 1
 ##     end
-    
+
 ##     y
 ## end
 
