b870d7d6b038bc51b3841ec5dd8a81737adba7d1
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-08-27 19:20:12 +0200

    Added randiv_ts

--- src/ModelsGenerators.jl
+++ src/ModelsGenerators.jl
@@ -1,7 +1,7 @@
 module ModelsGenerators
 
 export
-  randiv
+  randiv, randiv_ts
 
 # package code goes here
 function randiv(n::Int64        = 100;
@@ -9,10 +9,9 @@ function randiv(n::Int64        = 100;
                 k::Int64        = 1,
                 theta0::Float64 = 0.0,
                 rho::Float64    = 0.9,
-                CP::Int64       = 10)
+                CP::Int64       = 20)
 
     ## Generate IV Model with CP
-
     tau     = fill(sqrt(CP/(m*n)), m)
     z       = randn(n, m)
     vi      = randn(n, 1)
@@ -28,52 +27,50 @@ function randiv(n::Int64        = 100;
     return y, x, z
 end
 
-end # module
 
-
-## function typed_fad_hessian{T<:Real}(f::Function, ::Type{T})
-##   g(x::Vector{T}) = hessian(f(FADHessian(x)))
-##   return g
-## end
-
-## function ForwardDiff.hessian{T<:Real, n}(x::Array{FADHessian{T, n},1})
-
-##     y = Array(T, n, n, length(x))
-##     k = 1
-
-##     for s = 1:length(x)
-##         for i in 1:n
-##             for j in 1:i
-##                 y[i, j, s] = x[s].h[k]
-##                 k += 1
-##             end
-##         end
-##         for i in 1:n
-##             for j in (i+1):n
-##                 y[i, j, s] = y[j, i, s]
-##             end
-##         end
-##         k = 1
-##     end
-
-##     y
-## end
-
-## y,x,z = randiv(k=2)
-## h(theta) = (z'*(y-x*theta))[:]
-## h([.1,.1])
-
-## g = forwarddiff_jacobian(h, Float64, fadtype=:typed)
-## g([.1,.1])
-
-## fad =  hessian(f(FADHessian([.1, .1])))
+function randiv_ts(n::Int64; m::Int64 = 2, σ₁₂::Real = 0.9, ϕ::Real = .5, θ::Real = .5, β::Real = 1.0, burnin::Int64 = 1000)
+  ## Same design as in
+  ##
+  ## http://faculty.georgetown.edu/gk232/research/KGMM.pdf
+  ## Guido's parametrization
+  ## ρ ∈ {.1, .5, .9}
+  ## ϕ ∈ {.1, .3, .5}   # control quality instrument, low value weak
+  ## θ ∈ {-.9, -.5, 0. .5, .9}
 
 
 
-## f(x) = [x[1]^2+x[2], 3*x[1], x[1]^2*x[2]^3]
+  nt  = n+m+burnin
+  out = (burnin + 1):(n + burnin)
+  #    y <- u <- x <- v <- rep(0, nt + 1)
 
-## ss = forwarddiff_hessian(f, Float64, fadtype=:typed)
-## ss([.1, .1])
+  y    = Array(Float64, nt+1)
+  x    = Array(Float64, nt+1)
+  x[1] = 0.0
+  y[1] = 0.0
 
-## ss = forwarddiff_hessian(f, Float64, fadtype=:typed)
-## ss([.1, .1])
+  u₁  = randn(nt, 2)
+  ρ   = [1.0 σ₁₂; σ₁₂ 1.0]
+  u₁  = u₁*chol(ρ)
+
+  y[1] = β*x[1] + u₁[1, 2]
+
+  for j=2:nt
+    x[j] = ϕ * x[j-1] + u₁[j-1, 1]
+    y[j] = β*x[j] + u₁[j, 2] - θ*u₁[j-1, 2]
+  end
+
+  z = ones(n, 2*m+1)
+  for j = 2:m+1
+    z[:, j] = x[out-j]
+  end
+
+  for j = m+2:2*m+1
+    z[:, j] = y[out-j]
+  end
+
+  return y[out], [ones(n) x[out]], z
+
+end
+
+
+end
